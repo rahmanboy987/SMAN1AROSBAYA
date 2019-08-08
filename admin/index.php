32efddf848f382e62b0d538dab840bf6520f1dc4
Moh. Fathur Rohman <rahmanboy987@gmail.com>
2019-08-08 08:37:05 +0700

    update

--- admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  if(isset($_COOKIE['admin'])){
-    header('Location: dashboard.php');
-  }else{
-    header('Location: login.php');
-  }
-?>
--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("../_config/config.php");
-
-unset($_SESSION['user']);
-echo "<script>window.location='" . 'login.php' . "';</script>";
